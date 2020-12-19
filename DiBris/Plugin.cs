46e4bb2ba607b51091c8ebb9cb1f122616f3373d
Auros Nexus <aurosnex@gmail.com>
2020-12-19 13:47:52 -0500

    Initial commit

---
+++ DiBris/Plugin.cs
@@ -0,0 +1,27 @@
+﻿using IPA;
+using IPALogger = IPA.Logging.Logger;
+
+namespace DiBris
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        [Init]
+        public Plugin(IPALogger _)
+        {
+
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+
+        }
+    }
+}
